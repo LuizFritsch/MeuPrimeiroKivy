a57c1e50cac62ceddc1aebc991e887dcb8b90ee1
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2019-02-11 09:07:53 -0300

    Associando widgets

--- test.py
+++ test.py
@@ -1,8 +1,23 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 
 class Test(App):
 	def build(self):
-		return Button(text = "Olá mundo!")
+		box = BoxLayout(orientation='vertical')
+		button = Button(text = "Botao 1")
+		label = Label(text="Texto 1")
+		box.add_widget(button)
+		box.add_widget(label)
+
+		box2 = BoxLayout()
+		button2 = Button(text = "Botao 2")
+		label2 = Label(text="Texto 2")
+		box2.add_widget(button2)
+		box2.add_widget(label2)
+
+		box.add_widget(box2)
+		return box
 t = Test()
 t.run()
