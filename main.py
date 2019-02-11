cd6d4cabea34b68d2b04d5a159023bdab1e351e8
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2019-02-11 10:13:08 -0300

    Adicionando Widgets Dinamicamente e inicio com TextInput's

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
 
-class Tarefas(ScrollView):
+class Tarefas(BoxLayout):
 	def __init__(self, tarefas, **kwargs):
 		super().__init__(**kwargs)
 		for tarefa in tarefas:
 			self.ids.box.add_widget(Tarefa(text=tarefa))
+	def addWidget(self):
+		texto = self.ids.texto.text
+		self.ids.box.add_widget(Tarefa(text=texto))
+		self.ids.texto.text = ""
 
 class Tarefa (BoxLayout):
 	def __init__(self, text="", **kwargs):
