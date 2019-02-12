2e50039cd8d1f7f8882a40777c707d072bfb2ffb
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2019-02-12 10:10:38 -0300

    Botao voltar do Android

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.screenmanager import ScreenManager , Screen
+from kivy.core.window import Window
 
 class Gerenciador(ScreenManager):
     pass
@@ -16,6 +17,17 @@ class Tarefas(Screen):
 		super().__init__(**kwargs)
 		for tarefa in tarefas:
 			self.ids.box.add_widget(Tarefa(text=tarefa))
+
+	def on_pre_enter(self):
+		Window.bind(on_keyboard = self.voltar)
+
+	def on_pre_leave(self):
+		Window.unbind(on_keyboard = self.voltar)
+
+	def voltar(self, window,key,*args):
+		if key == 27:
+			App.get_running_app().root.current = "menu"
+		return True
 	def addWidget(self):
 		texto = self.ids.texto.text
 		self.ids.box.add_widget(Tarefa(text=texto))
